d0933d0d50a2118227805a406cf32ed91cb51832
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-07-18 23:47:53 +0800

    Initial commit

---
+++ src/test/java/pres/qianmuna/alg/InitTest.java
@@ -0,0 +1,23 @@
+package pres.qianmuna.alg;
+
+import org.junit.Test;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/7/18  23:36
+ * @description :
+ */
+public class InitTest {
+
+    @Test
+    public void m1(){
+
+    }
+
+    /// 数组 交集
+    public int[] intersect(int[] nums1, int[] nums2) {
+        return null;
+    }
+}
---
+++ src/test/kotlin/pres/qianmuna/alg/Init.kt
@@ -0,0 +1,9 @@
+package pres.qianmuna.alg
+
+/**
+@author HJC
+@date 2020/7/18  23:35
+@description :
+@version 1.0
+谦谦君子 卑以自牧也
+ */
