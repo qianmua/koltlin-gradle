9587458ad8e1bff8e42fd88e534b61c6b8cc992e
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-07-19 19:27:52 +0800

    note

---
+++ src/main/kotlin/pres/qianmuna/_base64_/Base.kt
@@ -0,0 +1,25 @@
+package pres.qianmuna._base64_
+
+import sun.misc.BASE64Decoder
+import sun.misc.BASE64Encoder
+import java.nio.charset.Charset
+
+/**
+@author HJC
+@date 2020/7/19  19:21
+@description :
+@version 1.0
+谦谦君子 卑以自牧也
+ */
+
+fun main() {
+    var decoder = BASE64Decoder()
+    var encoder = BASE64Encoder()
+    var string = String(decoder.decodeBuffer("yf3WsLzT0L0="), Charset.forName("GB2312"))
+    var string2 = String(decoder.decodeBuffer("5oiR5Y+8bm1k="), Charset.forName("UTF8"))
+    println(string)
+    println(string2)
+
+    var buffer = encoder.encodeBuffer("我叼nmd".toByteArray(Charset.forName("UTF8")))
+    println(buffer)
+}
