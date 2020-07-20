2a4274034455a7958a6747be1a3842067c1ccb8e
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-07-20 23:54:42 +0800

    note

--- src/test/kotlin/pres/qianmuna/study/Test1.kt
+++ src/test/kotlin/pres/qianmuna/study/Test1.kt
@@ -1,6 +1,7 @@
 package pres.qianmuna.study
 
 import org.junit.Test
+import kotlin.random.Random
 
 
 /**
@@ -36,6 +37,9 @@ class Test1 {
     interface Foo{
         // static
         companion object{
+
+//            const val ans2 = 100
+
             @JvmField
             val ans = 1
 
@@ -44,9 +48,60 @@ class Test1 {
                 println("hello world")
             }
         }
+
+
+//        @JvmDefault
+        fun m1(): Int = 100
+    }
+
+    @Test
+    // 实验性
+    @kotlin.ExperimentalUnsignedTypes
+    fun m3(){
+        val uint = 1000u
+        val ulong = 1000uL
+        val ulong2 = ulong.toLong()
+
+    }
+
+
+    @Test
+    fun m4(){
+        println(Random.nextInt(100))
+
+
+        //isNullOrEmpty
+        //boolean
+        // orEmpty
+        // 空容器
+
+        val map1 = {"a" to 1 ; "b" to 2; "c" to 2; "d" to 2; "e" to 2}
+
+        val key = 'a' .. 'c'
+        //.capitalize() 首字母 大写
+        val map = key.associateWith { it.toString().repeat(6).capitalize() }
+
+        map.forEach { println(it)  }
+
+        // ifEmpty
+        // ifBlank
+        val str = "        a      "
+        println(str.ifBlank { "blank" })
+
+
+    }
+
+    @Test
+    fun m5(){
+
+
+
     }
 
 
 
 
 }
+
+// 测试
+//inline class C2(val v1:String )
