f6daeab64827e918c9598534ce7936267d1824c4
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-08-10 15:07:40 +0800

    mbp

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,22 @@ dependencies {
     // org.springframework:spring-kafka:2.2.7.RELEASE
     implementation("org.springframework.kafka:spring-kafka:2.2.7.RELEASE")
 
+    // https://mvnrepository.com/artifact/com.baomidou/mybatis-plus-boot-starter
+//    compile group: 'com.baomidou', name: 'mybatis-plus-boot-starter', version: '3.3.2'
+    implementation("com.baomidou:mybatis-plus-boot-starter:3.3.2")
+
+    //org.springframework.boot:spring-boot-starter-web:$springBootVersion
+    implementation("org.springframework.boot:spring-boot-starter-web:2.2.6.RELEASE")
+    //org.mybatis.spring.boot:mybatis-spring-boot-starter:1.3.2
+    // mysql:mysql-connector-java:$mysql_version
+
+    // lombok
+//    implementation ( "org.projectlombok",  "lombok",  "1.18.10")
+    implementation ("org.projectlombok",  "lombok",  "1.18.10")
+
+//    implementation("org.projectlombok:lombok:1.1.8.RELEASE")
+
+
     //compile group: 'com.alibaba.csp', name: 'sentinel-core', version: '1.7.2'
     // https://mvnrepository.com/artifact/com.alibaba.nacos/nacos-client
     //compile group: 'com.alibaba.nacos', name: 'nacos-client', version: '1.3.1'
